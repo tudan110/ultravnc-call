e5026ad0191bb98abbb4c9ef2983de0c1646116b
tudan110 <wtudan@126.com>
2019-09-09 17:12:27 +0800

    测试类总是报错

--- ultravnc-session-manager/src/test/java/indi/tudan/uvnccall/sessionmanager/UltravncSessionManagerApplicationTests.java
+++ ultravnc-session-manager/src/test/java/indi/tudan/uvnccall/sessionmanager/UltravncSessionManagerApplicationTests.java
@@ -1,11 +1,9 @@
 package indi.tudan.uvnccall.sessionmanager;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class UltravncSessionManagerApplicationTests {
 
