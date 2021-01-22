51172950952f5a489cf68f4f59419c8883e444f3
tudan110 <wtudan@126.com>
2021-01-22 14:34:02 +0800

    命令行接口，启动 server

--- ultravnc-server-starter/src/main/java/indi/tudan/uvnccall/UltraVNCServerStarter.java
+++ ultravnc-server-starter/src/main/java/indi/tudan/uvnccall/UltraVNCServerStarter.java
@@ -18,43 +18,6 @@ public class UltraVNCServerStarter {
 
         Cli.startServer(args);
 
-        /*if (!Cli.startServer(args)) {
-
-            // 启动 UltraVNC Server
-            switch (args.length) {
-                case 1:
-
-                    // 90699
-                    VNCUtils.startUltraVNCServer(args[0]);
-                    break;
-                case 2:
-
-                    // 90699 "C:/Program Files/uvnc bvba/UltraVNC/winvnc.exe"
-                    VNCUtils.startUltraVNCServer(args[0], args[1]);
-                    break;
-                case 3:
-
-                    // 90699 "C:/Program Files/uvnc bvba/UltraVNC/winvnc.exe" 10.20.16.48
-                    VNCUtils.startUltraVNCServer(args[0], args[1], args[2]);
-                    break;
-                case 4:
-
-                    // 90699 "C:/Program Files/uvnc bvba/UltraVNC/winvnc.exe" 10.20.16.48 5500
-                    VNCUtils.startUltraVNCServer(args[0], args[1], args[2], args[3]);
-                    break;
-                default:
-                    StaticLog.error(ConfigConstants.START_ULTRAVNC_SERVER_NO_PARAMETER_INFO);
-                    break;
-            }
-
-        *//*VNCUtils.startUltraVNCServer(
-                "C:\\Program Files\\uvnc bvba\\UltraVNC\\winvnc.exe",,
-                "90699"
-                "10.20.16.48",
-                "5500");*//*
-
-        }*/
-
     }
 
 }
