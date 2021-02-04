9b778909c47149b76cd49b5f18056d12ee3c40c3
tudan110 <wtudan@126.com>
2021-02-04 11:34:03 +0800

    stop、start init

--- ultravnc-call-core/src/main/java/indi/tudan/uvnccall/cli/Cli.java
+++ ultravnc-call-core/src/main/java/indi/tudan/uvnccall/cli/Cli.java
@@ -136,4 +136,234 @@ public class Cli {
 
     }
 
+    public static boolean stopServer(String... args) {
+
+        // 创建一个解析器
+        CommandLineParser parser = new DefaultParser();
+
+        // 创建一个 Options，用来包装 option
+        Options options = new Options();
+
+        // help
+        options.addOption("h", "help", false, "Print help");
+
+        // 编号，唯一，必须是数字，且至少三位（即最小是 100，最大是 2147483647）
+        options.addOption(Option.builder("id")
+                .longOpt("id")
+                .hasArg()
+                .desc("编号，唯一，必须是数字，且至少三位（即最小是 100，最大是 2147483647）")
+                .build()
+        );
+
+        // vncviewer.exe 路径（可选参数）
+        options.addOption(Option.builder("dir")
+                .longOpt("directory")
+                .hasArg()
+                .desc("vncviewer.exe 路径（可选参数）")
+                .build()
+        );
+
+        // 中继器服务器 IP（可选参数）
+        options.addOption(Option.builder("ip")
+                .longOpt("ip")
+                .hasArg()
+                .desc("中继器服务器 IP（可选参数）")
+                .build()
+        );
+
+        // 中继器 UltraVNC Viewer 监听端口（可选参数）
+        options.addOption(Option.builder("p")
+                .longOpt("port")
+                .hasArg()
+                .desc("中继器 UltraVNC Viewer 监听端口（可选参数）")
+                .build()
+        );
+
+        String id = "";
+        String directory = "";
+        String ip = "";
+        String port = "";
+
+        try {
+
+            // 解析命令行参数
+            CommandLine line = parser.parse(options, args);
+
+            // 使用帮助
+            if (line.hasOption('h')) {
+
+                HelpFormatter hf = new HelpFormatter();
+                hf.setWidth(120);
+                // 打印使用帮助
+                hf.printHelp("ultravnc-call", options, true);
+
+                return false;
+
+            }
+
+            // 编号，唯一，必须是数字，且至少三位（即最小是 100，最大是 2147483647）
+            if (line.hasOption("id")) {
+                id = line.getOptionValue("id");
+            }
+
+            // vncviewer.exe 路径（可选参数）
+            if (line.hasOption("dir")) {
+                directory = line.getOptionValue("dir");
+            }
+
+            // 中继器服务器 IP（可选参数）
+            if (line.hasOption("ip")) {
+                ip = line.getOptionValue("ip");
+            }
+
+            // 中继器 UltraVNC Viewer 监听端口（可选参数）
+            if (line.hasOption("p")) {
+                port = line.getOptionValue("p");
+            }
+
+            if (StrUtil.isBlank(id)) {
+                StaticLog.error(ConfigConstants.START_ULTRAVNC_SERVER_NO_PARAMETER_INFO);
+                return false;
+            }
+            if (StrUtil.isAllBlank(directory, ip, port)) {
+                VNCUtils.startUltraVNCServer(id);
+            } else if (StrUtil.isNotBlank(directory)
+                    && StrUtil.isAllBlank(ip, port)) {
+                VNCUtils.startUltraVNCServer(id, directory);
+            } else if (StrUtil.isNotBlank(directory)
+                    && StrUtil.isNotBlank(ip)
+                    && StrUtil.isBlank(port)) {
+                VNCUtils.startUltraVNCServer(id, directory, ip);
+            } else if (StrUtil.isAllNotBlank(id, directory, ip, port)) {
+                VNCUtils.startUltraVNCServer(id, directory, ip, port);
+            } else {
+                StaticLog.error(ConfigConstants.START_ULTRAVNC_SERVER_NO_PARAMETER_INFO);
+                return false;
+            }
+
+            return true;
+
+        } catch (ParseException e) {
+            StaticLog.info("Unexpected exception:" + e.getMessage());
+        }
+
+        return false;
+
+    }
+
+    public static boolean startViewer(String... args) {
+
+        // 创建一个解析器
+        CommandLineParser parser = new DefaultParser();
+
+        // 创建一个 Options，用来包装 option
+        Options options = new Options();
+
+        // help
+        options.addOption("h", "help", false, "Print help");
+
+        // 编号，唯一，必须是数字，且至少三位（即最小是 100，最大是 2147483647）
+        options.addOption(Option.builder("id")
+                .longOpt("id")
+                .hasArg()
+                .desc("编号，唯一，必须是数字，且至少三位（即最小是 100，最大是 2147483647）")
+                .build()
+        );
+
+        // vncviewer.exe 路径（可选参数）
+        options.addOption(Option.builder("dir")
+                .longOpt("directory")
+                .hasArg()
+                .desc("vncviewer.exe 路径（可选参数）")
+                .build()
+        );
+
+        // 中继器服务器 IP（可选参数）
+        options.addOption(Option.builder("ip")
+                .longOpt("ip")
+                .hasArg()
+                .desc("中继器服务器 IP（可选参数）")
+                .build()
+        );
+
+        // 中继器 UltraVNC Viewer 监听端口（可选参数）
+        options.addOption(Option.builder("p")
+                .longOpt("port")
+                .hasArg()
+                .desc("中继器 UltraVNC Viewer 监听端口（可选参数）")
+                .build()
+        );
+
+        String id = "";
+        String directory = "";
+        String ip = "";
+        String port = "";
+
+        try {
+
+            // 解析命令行参数
+            CommandLine line = parser.parse(options, args);
+
+            // 使用帮助
+            if (line.hasOption('h')) {
+
+                HelpFormatter hf = new HelpFormatter();
+                hf.setWidth(120);
+                // 打印使用帮助
+                hf.printHelp("ultravnc-call", options, true);
+
+                return false;
+
+            }
+
+            // 编号，唯一，必须是数字，且至少三位（即最小是 100，最大是 2147483647）
+            if (line.hasOption("id")) {
+                id = line.getOptionValue("id");
+            }
+
+            // vncviewer.exe 路径（可选参数）
+            if (line.hasOption("dir")) {
+                directory = line.getOptionValue("dir");
+            }
+
+            // 中继器服务器 IP（可选参数）
+            if (line.hasOption("ip")) {
+                ip = line.getOptionValue("ip");
+            }
+
+            // 中继器 UltraVNC Viewer 监听端口（可选参数）
+            if (line.hasOption("p")) {
+                port = line.getOptionValue("p");
+            }
+
+            if (StrUtil.isBlank(id)) {
+                StaticLog.error(ConfigConstants.START_ULTRAVNC_SERVER_NO_PARAMETER_INFO);
+                return false;
+            }
+            if (StrUtil.isAllBlank(directory, ip, port)) {
+                VNCUtils.startUltraVNCServer(id);
+            } else if (StrUtil.isNotBlank(directory)
+                    && StrUtil.isAllBlank(ip, port)) {
+                VNCUtils.startUltraVNCServer(id, directory);
+            } else if (StrUtil.isNotBlank(directory)
+                    && StrUtil.isNotBlank(ip)
+                    && StrUtil.isBlank(port)) {
+                VNCUtils.startUltraVNCServer(id, directory, ip);
+            } else if (StrUtil.isAllNotBlank(id, directory, ip, port)) {
+                VNCUtils.startUltraVNCServer(id, directory, ip, port);
+            } else {
+                StaticLog.error(ConfigConstants.START_ULTRAVNC_SERVER_NO_PARAMETER_INFO);
+                return false;
+            }
+
+            return true;
+
+        } catch (ParseException e) {
+            StaticLog.info("Unexpected exception:" + e.getMessage());
+        }
+
+        return false;
+
+    }
+
 }
